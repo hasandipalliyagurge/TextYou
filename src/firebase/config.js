86a15ee4aea88e8a6616cd5af6b3b53d85674c0d
hasandi_palliaguruge <pghasandi@gmail.com>
2020-07-20 23:36:24 +0530

    firebase connection

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,10 @@
 import Firebase from 'firebase';
 
 const firebaseConfig= { 
-    apiKey:'',
-    databseURL:'',
-    projectId:'',
-    appId:'',
+    apiKey:'AIzaSyC_wOBYpL6ezayY08FFA7vs5seyTPNtCC8',
+    databseURL:'https://textyou-7a1ba.firebaseio.com/',
+    projectId:'textyou-7a1ba',
+    appId:'1:391093870982:android:512d7d5aac158700378c07',
 };
 
 export default Firebase.initializeApp(firebaseConfig);
