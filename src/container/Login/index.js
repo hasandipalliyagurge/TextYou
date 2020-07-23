d3e32a4901b857da8c1c515b647b15d6babdf2fd
hasandi_palliaguruge <pghasandi@gmail.com>
2020-07-23 08:13:13 +0530

    login

--- src/container/Login/index.js
+++ src/container/Login/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import {SafeAreaView, Text } from 'react-native';
-
+import {SafeAreaView, Text, View } from 'react-native';
+import {globalStyle, color } from '../../utility';
+import Logo from '../../component/logo';
+import InputField from '../../component/input';
+import RoundCornerButton from '../../component/button/RoundCornerButton';
+//component\logo
 const Login= ({navigation}) => {
     return (
-        <SafeAreaView>
-            <Text onPress={() => navigation.navigate('SignUp')}>Login</Text>
+        <SafeAreaView style={[globalStyle.flex1, {backgroundColor: color.BLACK}]}>
+            <View style={[globalStyle.flex1, {backgroundColor: color.BLACK}]}>
+                <Logo/>
+
+            </View>
+            <View style={[globalStyle.flex2, globalStyle.sectionCentered]}>
+                <InputField placeholder="Enter Email" />
+                <InputField placeholder="Enter Password" secureTextEntry={true}/>
+                <RoundCornerButton title="Login" />
+            </View>
         </SafeAreaView>
     );
 };
 
-export default Login; 
+export default Login;
